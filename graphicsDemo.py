02007ad1730145e326655367ddc7fb10a08eb989
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-20 13:43:02 -0400

    Updated from Brython Server: 9/20/2017 1:42:58 PM

--- graphicsDemo.py
+++ graphicsDemo.py
@@ -12,7 +12,8 @@ black = Color(0x000000,1)
 blackOutline = LineStyle(1,black) #pixels,color
 
 redRectangle = RectangleAsset(200,100,blackOutline,red) #width height outline fill
-
+blueCircle = CircleAsset(50, blackOutline, blue)
 
 Sprite(redRectangle)
+Sprite(blueCircle, (50,50))
 App().run()
