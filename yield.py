7f174a5b75272db8e01883a4ce309b071c5911d8
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-22 14:54:13 -0400

    Updated from Brython Server: 9/22/2017 2:54:11 PM

--- yield.py
+++ yield.py
@@ -1,1 +1,12 @@
+#Matthew Siegel
+#9/20/17
+#yield.py - writing people's name
 
+from ggame import *
+
+red = Color(0xFF0000,1)
+green = Color(0x00FF00,1)
+blue = Color(0x0000FF,1)
+black = Color(0x000000,1)
+
+blackOutline = LineStyle(1,black) #pixels,color
