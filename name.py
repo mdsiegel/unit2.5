cfb68423b346eb41256626da0caf30fb846b4caa
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-22 14:41:09 -0400

    Updated from Brython Server: 9/22/2017 2:41:07 PM

--- name.py
+++ name.py
@@ -1,1 +1,14 @@
+#Matthew Siegel
+#9/20/17
+#name.py - writing people's name
 
+from ggame import *
+
+name = input('Enter you name: ')
+colorCode = input('Enter a hex RGB code: ')
+colorinput = Color(colorCode,1)
+
+text = TextAsset(name,fill=colorinput,style='40pt Times')
+
+Sprite(text)
+App().run()
