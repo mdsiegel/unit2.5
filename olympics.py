ab671ef621fc67e547b310235481c9fabfd90882
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-26 14:29:02 -0400

    Updated from Brython Server: 9/26/2017 2:28:58 PM

--- olympics.py
+++ olympics.py
@@ -1,1 +1,15 @@
+#Matthew Siegel
+#9/20/17
+#olympics.py - Making the olympic rings
+
+red = Color(0xFF0000,1)
+green = Color(0x00FF00,1)
+blue = Color(0x0000FF,1)
+black = Color(0x000000,1)
+
+blueCircle = CircleAsset(50, blackOutline, blue)
+blueCircle = CircleAsset(50, blackOutline, blue)
+blueCircle = CircleAsset(50, blackOutline, blue)
+blueCircle = CircleAsset(50, blackOutline, blue)
+blueCircle = CircleAsset(50, blackOutline, blue)
 
