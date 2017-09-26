ede2a4121d67b16e319b96856a86e395193055fd
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-26 14:29:19 -0400

    Updated from Brython Server: 9/26/2017 2:29:16 PM

--- olympics.py
+++ olympics.py
@@ -6,6 +6,8 @@ red = Color(0xFF0000,1)
 green = Color(0x00FF00,1)
 blue = Color(0x0000FF,1)
 black = Color(0x000000,1)
+yellow = Color(0xffff00,1)
+
 
 blueCircle = CircleAsset(50, blackOutline, blue)
 blueCircle = CircleAsset(50, blackOutline, blue)
