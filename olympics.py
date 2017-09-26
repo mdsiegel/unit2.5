d0f36c9aef37b50528607c08ab75d9ea50512419
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-26 14:38:34 -0400

    Updated from Brython Server: 9/26/2017 2:38:31 PM

--- olympics.py
+++ olympics.py
@@ -2,16 +2,22 @@
 #9/20/17
 #olympics.py - Making the olympic rings
 
+from ggame import *
+
 red = Color(0xFF0000,1)
 green = Color(0x00FF00,1)
 blue = Color(0x0000FF,1)
 black = Color(0x000000,1)
 yellow = Color(0xffff00,1)
+white = Color(0xffffff,1)
 
 
 blueCircle = CircleAsset(50, blackOutline, blue)
-yellowCircle = CircleAsset(50, blackOutline, blue)
-blackCircle = CircleAsset(50, blackOutline, blue)
-GreenCircle = CircleAsset(50, blackOutline, blue)
-RedCircle = CircleAsset(50, blackOutline, blue)
+whiteCircle = CircleAsset(50, blackOutline, white)
+yellowCircle = CircleAsset(50, blackOutline, yellow)
+blackCircle = CircleAsset(50, blackOutline, black)
+GreenCircle = CircleAsset(50, blackOutline, green)
+RedCircle = CircleAsset(50, blackOutline, red)
+
+
 
